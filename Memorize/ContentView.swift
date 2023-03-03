daed8f74251fd14e1ba52168bddd2b97e8d39199
EsraaMo <em@next-munich.com>
2023-03-03 09:28:01 +0100

    [Lecture2_45:00] create card view and add it to list of cards

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Esraa Moustafa on 03/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis = ["🚘", "✈️", "🚆", "🚌"]
+    
+    var body: some View {
+        HStack {
+            ForEach(emojis, id: \.self) { emoji in
+                CardView(content: emoji)
+            }
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+    
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Esraa Moustafa on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
