fcb294dd0f439d258fd1a7f3b2e2df5e56566598
EsraaMoustafa <esraa.moustafa.9@gmail.com>
2023-03-14 09:12:53 +0100

    [Lecture-2] finished * make cards count flexible * add plus and minus buttons with add and remove actions * convert the HStack of cards Views to Scrollable LazyVGrid * use strokeBorder instead of stroke to make the stroke fill the inner borders only

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["🚘", "✈️", "🚆", "🚌"]
+    var emojis = ["🚘", "✈️", "🚆", "🚌", "🚒",
+                  "🚛", "🦼", "🛵", "🚔", "🚞",
+                  "🚠", "🚋", "🚄", "🚝", "🚈",
+                  "🚊", "🚉", "🛫", "🛬", "💺",
+                  "⛴", "🚢", "🛶", "🚤", "🛳",
+                  "🚲"]
+    
+    @State var emojiCount = 4
     
     var body: some View {
-        HStack {
-            ForEach(emojis, id: \.self) { emoji in
-                CardView(content: emoji)
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
             }
+            .foregroundColor(.red)
+
+            Spacer()
+            
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
         }
-        .padding(.horizontal)
-        .foregroundColor(.red)
+        .padding()
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
@@ -31,7 +72,7 @@ struct CardView: View {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content).font(.largeTitle)
             } else {
                 shape.fill()
--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Stanford CS193p to learn SwiftUI
 * @state it change the variable to pointer and the change is done to this place where state refer to 
 * we don't use state to build real logic it's just small logic to save the current state 
 * if you use the same id for creating different views using ForEach it will create same view twice so if you click on one to do some actions it will do the same of the other `ForEach(emojis, id: \.self)`
+
+* Spacer() take all space that is not needed by any other viewBuilder
+* HStack use all space it can in all directions same for VStack 
+* LazyVGrid use all space it can use for vertical grid items but small space as possible for the horizontal to fit as much as it can for all elements 
+    * LazyVGrid is lazy about accessing the (body vars)"var body: some View" of all it's views,
+     we only get value of a "body var" in LazyVGrid for views that actually appears in screen.
+    * creating view is light wight, because accessing vars in view is simple
+     but accessing the "body var" will cause it to access the "body vars" of it's inner views.
+    * LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) 
+    takes the number of GridItem() that is needed to be fit in vertical space
+    * LazyVGrid(columns: GridItem(.adaptive(minimum: 20, maximum: <#T##CGFloat#>)))
+    customize the GridItem width to minimum size and fill the vertical space with as much as GridItem it can fill.
